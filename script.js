0076ba161d24488115cf230b0cc772339dc6cf0b
Brittany Doss <brinidoss@gmail.com>
2021-04-26 18:37:21 -0400

    inital commit

---
+++ script.js
@@ -0,0 +1,31 @@
+//Area of Circle
+
+function getAreaOfCircle(radius) {
+    return 3.14 * radius * radius;
+}
+
+console.log(getAreaOfCircle(5));
+
+//Circumference of Circle
+
+function getCircumferenceOfCircle(radius2) {
+    return 2 * 3.14 * radius2;
+}
+
+console.log(getCircumferenceOfCircle(5));
+
+//Area of Square
+
+function getAreaOfSquare(side) {
+    return side * side;
+}
+
+console.log(getAreaOfSquare(5));
+
+//Area of Triangle
+
+function getAreaOfTriangle(base, height) {
+    return base * height * .5;
+}
+
+console.log(getAreaOfTriangle(5, 5));
